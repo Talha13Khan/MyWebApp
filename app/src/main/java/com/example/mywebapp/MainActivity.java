b1600e01e17954bc1518c60525446722b95042cf
khatalha <khatalha2015@gmail.com>
2022-03-18 01:11:01 +0530

    First commit

---
+++ app/src/main/java/com/example/mywebapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mywebapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+     private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String url="https://www.youtube.com/c/SharadNehaGupta";
+        webView=findViewById(R.id.webview);
+
+        webView.loadUrl(url);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.canGoBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
